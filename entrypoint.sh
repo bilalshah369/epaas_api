989dfa3422a055dd747c839f0f7d56cb1b978db0
bilalshah369 <bilalshah369@yahoo.com>
2026-05-18 11:36:21 +0530

    tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 echo "Resetting database and applying migrations..."
 npx prisma migrate reset --force --skip-seed
 
-echo "Seeding database..."
-npm run db:seed || true
+echo "Seeding database...dfdf"
+npm run db:seed || true  
 
 echo "Starting server..."
 exec "$@"
